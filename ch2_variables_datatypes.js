0c4710b6cfbb0ef1d90d5a49d6d41bfed6ad9f7e
Ismail <nintendosuhaib@gmail.com>
2025-08-24 18:11:12 +0100

    Add files via upload

---
+++ ch2_variables_datatypes.js
@@ -0,0 +1,9 @@
+var firstName = "ismail"
+var secondName = "mohamed"
+var age = 14
+var isStudent = true
+console.log("firstName");
+console.log("firstName");
+console.log("firstName");
+console.log("firstName");
+console.log("firstName");
