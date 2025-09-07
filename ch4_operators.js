3e82cfa6102e92abf64a928d9c9424389b7fb2a7
Ismail <nintendosuhaib@gmail.com>
2025-09-07 17:56:07 +0100

    Add files via upload

---
+++ ch4_operators.js
@@ -0,0 +1,39 @@
+// operation.js
+
+let a = 12;
+let b = 4;
+
+console.log("a =", a, "b =", b);
+
+console.log("Addition: ", a + b);       // 16
+console.log("Subtraction: ", a - b);    // 8
+console.log("Multiplication: ", a * b); // 48
+console.log("Division: ", a / b);       // 3
+console.log("Modulus: ", a % b);        // 0 (remainder)
+
+// comparison.js
+
+let x = 10;
+let y = 5;
+
+console.log("x =", x, "y =", y);
+
+console.log("x == y :", x == y);   // false (checks equality)
+console.log("x != y :", x != y);   // true  (not equal)
+console.log("x > y  :", x > y);    // true
+console.log("x < y  :", x < y);    // false
+console.log("x >= y :", x >= y);   // true
+console.log("x <= y :", x <= y);   // false
+
+// logical.js
+
+let at= true;
+let be = false;
+
+console.log("a =", a, "b =", b);
+
+console.log("a && b :", a && b);  // false (AND → both must be true)
+console.log("a || b :", a || b);  // true  (OR  → at least one is true)
+console.log("!a     :", !a);      // false (NOT → reverses true/false)
+console.log("!b     :", !b);      // true
+
